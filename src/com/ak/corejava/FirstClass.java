013f3e580413e6a40e36afc4ad86f80b579bd26a
Aravindkumar7875 <aravindkumar6070@gmail.com>
2025-07-04 12:54:58 +0530

    fitajava

---
+++ src/com/ak/corejava/FirstClass.java
@@ -0,0 +1,31 @@
+package com.ak.corejava;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		boolean f = false;
+		
+		byte b = 127;
+		
+	
+		String abc = "Hi Hello";
+		
+		System.out.println(abc);
+		
+		//modulus % operator gives reminder 
+		//divide / operator gives quotient
+		int num = 10;
+		
+		int res = num%2;
+		System.out.println(res);
+		
+		if(num%2 ==0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+	}
+
+}
